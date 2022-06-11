192004d91b5d01cf8752228c443a9cf0e05fca97
Luciano <lucianov.dev@gmail.com>
2022-06-11 20:20:35 +0800

    nav links

--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -1,17 +1,14 @@
 import Link from 'next/link';
 import s from './Navbar.module.css';
-
 import Logo from 'components/icons/Logo';
 import { useUser } from 'utils/useUser';
-import { supabaseClient } from '@supabase/supabase-auth-helpers/nextjs';
 import { useRouter } from 'next/router';
+import cn from 'clsx';
 import ProfileDropdown from '../ProfileDropdown/ProfileDropdown';
 
 const Navbar = () => {
-  const { user, isLoading } = useUser();
   const router = useRouter();
-
-  console.log('user data', user);
+  const { user } = useUser();
 
   return (
     <nav className={s.root}>
@@ -19,7 +16,7 @@ const Navbar = () => {
         Skip to content
       </a>
       <div className="mx-auto max-w-7xl px-6">
-        <div className="flex justify-around align-center flex-row py-4 md:py-6 relative">
+        <div className="flex items-center flex-row py-4 md:py-6 relative">
           <div className="flex flex-1 items-center">
             <Link href="/">
               <a className={s.logo} aria-label="Logo">
@@ -28,28 +25,24 @@ const Navbar = () => {
             </Link>
           </div>
 
-          <nav className="space-x-12">
-            <Link href="/">
-              <a className={s.link}>About</a>
-            </Link>
-            <Link href="/">
-              <a className={s.link}>Solutions</a>
-            </Link>
-            <Link href="/pricing">
-              <a className={s.link}>Plans</a>
-            </Link>
-            {/* <Link href="/account">
-              <a className={s.link}>Account</a>
-            </Link> */}
+          <nav className="">
+            <ActiveLink href="/Platform">Platform</ActiveLink>
+            <ActiveLink href="/Platform">Devices</ActiveLink>
+            <ActiveLink href="/solutions">Solutions</ActiveLink>
+            <ActiveLink href="/Resources">Resources</ActiveLink>
+            <ActiveLink href="/pricing">Pricing</ActiveLink>
           </nav>
-          <div className="lg:flex flex-1 justify-end space-x-8 hidden">
+          <div className="lg:flex flex-1 justify-end space-x-6 hidden">
             {user ? (
               <ProfileDropdown user={user?.email} />
             ) : (
               <>
                 <Link href="/signin">
-                  <a className={s.link}>Sign in</a>
+                  <a className={s.link}>Log in</a>
                 </Link>
+                <button className="bg-[#A9FFF1]/10 px-5 py-3 text-xs font-semibold uppercase tracking-wide text-[#A9FFF1]">
+                  Get started
+                </button>
               </>
             )}
           </div>
@@ -60,3 +53,33 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+interface Props {
+  href: string;
+}
+
+export const ActiveLink: React.FC<Props> = ({ children, href }) => {
+  const router = useRouter();
+  const isActive = router.pathname === href;
+
+  const handleClick = (e: any) => {
+    e.preventDefault();
+    router.push(href);
+  };
+
+  return (
+    <a
+      href={href}
+      onClick={handleClick}
+      className={cn(
+        'px-6 py-2 font-semibold text-xs uppercase tracking-wide transition duration-300',
+        {
+          'text-[#A9FFF1]': isActive,
+          'text-zinc-200 hover:text-white': !isActive
+        }
+      )}
+    >
+      {children}
+    </a>
+  );
+};
